9d23b3d920365f03026bfcbd0c25920bc6a8cae8
Cyril <cyril.hohl@studentfr.ch>
2026-02-06 11:15:27 +0100

    test erreur

--- app/server.js
+++ app/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
+var erreur="erreur";
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
